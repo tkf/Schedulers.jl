9897f5e6062403ced042e8211e4aa9dfc7881fa7
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-03 14:27:22 -0800

    Run full benchmarks in test (for now) (#20)

--- test/SchedulersTests/src/test_benchmarks.jl
+++ test/SchedulersTests/src/test_benchmarks.jl
@@ -1,13 +1,16 @@
 module TestBenchmarks
 
 using Schedulers.Internal: Debug
-using SchedulersBenchmarks: clear, setup_smoke
+using SchedulersBenchmarks: clear, setup
 using Test
 
 function test_smoke_test_benchmarks()
     try
         local suite
+        #=
         @test (suite = setup_smoke()) isa Any
+        =#
+        @test (suite = setup()) isa Any
         Debug.get_logger() === nothing || return
         @test run(suite) isa Any
     finally
