6a08812694df3ee9251e16e5375c84a5cc403cf2
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-03 14:16:59 -0800

    Workaround copy-stack (#22)

--- benchmark/SchedulersBenchmarks/src/bench_fib.jl
+++ benchmark/SchedulersBenchmarks/src/bench_fib.jl
@@ -1,6 +1,7 @@
 module BenchFib
 
 import Schedulers
+using Schedulers.Internal: WorkStealingScheduler, WorkerStates
 using BenchmarkTools
 
 module SchedulersFib
@@ -22,17 +23,53 @@ function fib(n::Int)
 end
 end # module BaseFib
 
+function unsafe_gc!(
+    scheduler::WorkStealingScheduler = Schedulers.Internal.DEFAULT_WORKSTEALING_SCHEDULER_HANDLE[],
+)
+    nspins = 0
+    for worker in scheduler.workers
+        while (@atomic :monotonic worker.state) != WorkerStates.WAITING
+            yield()
+            nspins += 1
+            if nspins > 1_000_000
+                return
+            end
+        end
+    end
+    for deque in scheduler.queues
+        data = deque.buffer.data
+        n = length(data)
+        empty!(data)
+        resize!(data, n)
+    end
+end
+
 function setup(; ns = [20])
     suite = BenchmarkGroup()
     for n in ns
         sn = suite["n=$n"] = BenchmarkGroup()
-        sn["scheduler=:workstealing"] = @benchmarkable Schedulers.global_workstealing() do
-            SchedulersFib.fib($n)
-        end
-        sn["scheduler=:prioritized"] = @benchmarkable Schedulers.global_prioritized() do
-            SchedulersFib.fib($n)
-        end
-        sn["scheduler=:base"] = @benchmarkable BaseFib.fib($n)
+        sn["scheduler=:workstealing"] = @benchmarkable(
+            Schedulers.global_workstealing() do
+                SchedulersFib.fib($n)
+            end,
+            # Calling GC manually to avoid hitting copy-stack
+            setup = begin
+                unsafe_gc!()
+                GC.gc(false)
+            end
+        )
+        sn["scheduler=:prioritized"] = @benchmarkable(
+            Schedulers.global_prioritized() do
+                SchedulersFib.fib($n)
+            end,
+            # Calling GC manually to avoid hitting copy-stack
+            setup = GC.gc(false)
+        )
+        sn["scheduler=:base"] = @benchmarkable(
+            BaseFib.fib($n),
+            # To be equivalent to what other benchmarks do:
+            setup = GC.gc(false),
+        )
     end
     return suite
 end
